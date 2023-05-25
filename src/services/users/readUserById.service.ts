d25fa0d2265c7c74fc5283bb80feeb45e83a9fed
Gabriela <gabrielagarciafontoura@gmail.com>
2023-05-25 19:17:46 -0300

    fix: omit password

--- src/services/users/readUserById.service.ts
+++ src/services/users/readUserById.service.ts
@@ -2,13 +2,15 @@ import { Repository } from "typeorm";
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities";
 import { tUserReturn } from "../../interfaces/users.interfaces";
+import { returnUserSchema } from "../../schemas/users.schema";
 
 
-export const readUserByIdService = async (userId: number): Promise<tUserReturn | null>  => {
+export const readUserByIdService = async (userId: number): Promise<tUserReturn>  => {
 
     const userRepository: Repository<User> = AppDataSource.getRepository(User)
-    const findUser: User | null = await userRepository.findOneBy({id: userId});
+    const findUser = await userRepository.findOneBy({id: userId});
 
-    return findUser
-
+    const user:tUserReturn = returnUserSchema.parse(findUser)
+    
+    return user
 }
